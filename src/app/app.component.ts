8043e59b3f19c087aeb8111345d57a19bc569a3e
LeoLogisys <leoLogisys@gmail.com>
2023-05-16 17:51:31 -0500

    segundo commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,26 @@
 import { Component } from '@angular/core';
+import {FlatTreeControl} from '@angular/cdk/tree';
+import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
+
+interface FoodNode {
+  name: string;
+  children?: FoodNode[];
+}
+
+const TREE_DATA: FoodNode[] = [
+  {
+    name: 'Hoy',
+    children: [{name: 'Tarea1'}, {name: 'Tarea2'}, {name: 'Tarea 3'}],
+  }
+];
+
+interface ExampleFlatNode {
+  expandable: boolean;
+  name: string;
+  level: number;
+}
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +29,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SistemaHospital-Frontend';
+
+  private _transformer = (node: FoodNode, level: number) => {
+    return {
+      expandable: !!node.children && node.children.length > 0,
+      name: node.name,
+      level: level,
+    };
+  };
+
+  treeControl = new FlatTreeControl<ExampleFlatNode>(
+    node => node.level,
+    node => node.expandable,
+  );
+
+  treeFlattener = new MatTreeFlattener(
+    this._transformer,
+    node => node.level,
+    node => node.expandable,
+    node => node.children,
+  );
+
+  dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
+
+  constructor() {
+    this.dataSource.data = TREE_DATA;
+  }
+
+  hasChild = (_: number, node: ExampleFlatNode) => node.expandable;
 }
+
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 
 @NgModule({
@@ -9,7 +18,16 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     CommonModule
   ],
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatChipsModule,
+    MatTabsModule,
+    MatTreeModule,
+    MatMenuModule,
+    MatGridListModule
   ]
 })
 export class MaterialModule { }
